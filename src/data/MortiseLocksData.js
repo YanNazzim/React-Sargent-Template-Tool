9c52ef8374a118784da4680723a2c9f4aaf3c2cf
Yan <69820754+YanNazzim@users.noreply.github.com>
2024-09-30 23:17:07 -0400

    Filled in most of the mortise templates

--- src/data/MortiseLocksData.js
+++ src/data/MortiseLocksData.js
@@ -1,65 +1,72 @@
 /* Device Images */
-import Mortise7800 from '../images/MortiseLock7800.png'
+import Mortise7800 from "../images/MortiseLock7800.png";
 import Mortise8200 from "../images/Mortise Lock.png";
-import Mortise8200Electric from '../images/MortiseElectrified8200.png'
-import Roseless from '../images/MortiseRoselessTrim.png'
-import Mortise9200 from '../images/MortiseLock9200.png'
-import IndicatorSA190 from '../images/MortiseIndicatorSA190.png'
+import Mortise8200Electric from "../images/MortiseElectrified8200.png";
+import Roseless from "../images/MortiseRoselessTrim.png";
+import Mortise9200 from "../images/MortiseLock9200.png";
+import IndicatorSA190 from "../images/MortiseIndicatorSA190.png";
+import IndicatorVN1 from "../images/MortiseIndicatorVN1.png";
+import Strike9200 from '../images/Strikes 9200.png'
 
 export const MortiseLocks = {
   8200: [
     {
       device: "Standard",
-      functions: '8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268',
+      functions:
+        "8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268",
       title: "W/ Sectional Trims",
       image: Mortise8200,
       text: "View Mounting Template Doc# 7057-30",
       link: "https://storage.googleapis.com/aa-americas/dam/AADSS1055660",
       text1: "View (50-) Mounting Template Doc# 7682-A",
       link1: "https://storage.googleapis.com/aa-americas/dam/AADSS1056033",
-      text2: 'View 78-0034 Deadbolt Strike Template (03, 20, 21, 22, 23 Functions) Doc# 7064-C',
-      link2: 'https://storage.googleapis.com/aa-americas/dam/AADSS1055661',
-      text3: 'View THumbturn Template Doc# 7059-B',
-      link3: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056032',
+      text2:
+        "View 78-0034 Deadbolt Strike Template (03, 20, 21, 22, 23 Functions) Doc# 7064-C",
+      link2: "https://storage.googleapis.com/aa-americas/dam/AADSS1055661",
+      text3: "View THumbturn Template Doc# 7059-B",
+      link3: "https://storage.googleapis.com/aa-americas/dam/AADSS1056032",
     },
     {
       device: "Standard",
-      functions: '8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268',
+      functions:
+        "8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268",
       title: "W/ Escutcheon Trims",
       image: Mortise8200,
       text: "View Mounting Template Doc# 7057-30",
       link: "https://storage.googleapis.com/aa-americas/dam/AADSS1055660",
-      text1: "View LE1, LE3, LW1, CE, TE, KE1, KE3, KW1 Trim Template Doc# 7052-E",
+      text1:
+        "View LE1, LE3, LW1, CE, TE, KE1, KE3, KW1 Trim Template Doc# 7052-E",
       link1: "https://storage.googleapis.com/aa-americas/dam/AADSS1056024",
       warning: "",
       text2: "View LE2, LE4, KE2, KE4 Trim Template Doc# 7053-D",
       link2: "https://storage.googleapis.com/aa-americas/dam/AADSS1056025",
       text3: "View LS & KS Trim Template Doc# 7054-C",
       link3: "https://storage.googleapis.com/aa-americas/dam/AADSS1056043",
-      text4: 'View WT Trim Template Doc# 7055-B',
-      link4: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056027',
+      text4: "View WT Trim Template Doc# 7055-B",
+      link4: "https://storage.googleapis.com/aa-americas/dam/AADSS1056027",
     },
     {
-      device: 'Standard',
-      functions: '8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268',
-      title: 'Roseless (R8200)',
+      device: "Standard",
+      functions:
+        "8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268",
+      title: "Roseless (R8200)",
       image: Roseless,
-      text: 'View Roseless Mortise Template Doc# 4647-B',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056034',
-
+      text: "View Roseless Mortise Template Doc# 4647-B",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1056034",
     },
     {
-      device: 'Standard',
-      functions: '8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268',
-      title: 'Roseless (R8200)',
+      device: "Standard",
+      functions:
+        "8204, 8205, 8206, 8213, 8215, 8216, 8217, 8224, 8225, 8226, 8227, 8228, 8229, 8230, 8231, 8235, 8236, 8237, 8238, 8239, 8240, 8241, 8242, 8243, 8245, 8246, 8247, 8248, 8249, 8250, 8251, 8252, 8255, 8256, 8257, 8258, 8259, 8265, 8266, 8267, 8268",
+      title: "Roseless (R8200)",
       image: Roseless,
-      text: 'View Roseless Mortise Template Doc# 4647-B',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056034',
-
+      text: "View Roseless Mortise Template Doc# 4647-B",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1056034",
     },
     {
       device: "Electrified",
-      functions: '8270, 8271, 8272, 8273, 82270, 82271, 82274, 82275, 82280, 82281, 82284, 82285',
+      functions:
+        "8270, 8271, 8272, 8273, 82270, 82271, 82274, 82275, 82280, 82281, 82284, 82285",
       title: "8200 Mortise Lock - Electrified",
       image: Mortise8200Electric,
       text: "Download Electrified Template",
@@ -69,65 +76,57 @@ export const MortiseLocks = {
       link1: "https://storage.googleapis.com/aa-americas/dam/AADSS1056016",
     },
     {
-      device: 'Indicator',
-      functions: "8220, 8221, 8222, 8224, 8225, 8226, 8228, 8229, 8230, 8239, 8240, 8241, 8243, 8245, 8246, 8247, 8250, 8251, 8252, PHR-NAC-82281",
-      title: 'SA190 - Indicator',
+      device: "Indicator",
+      warning:
+        "Right Click on picture -> Open image in new tab to see more details",
+      functions:
+        "8220, 8221, 8222, 8224, 8225, 8226, 8228, 8229, 8230, 8239, 8240, 8241, 8243, 8245, 8246, 8247, 8250, 8251, 8252, PHR-NAC-82281",
+      title:
+        "Sectional Indicator SA190, SA191, SA192, SA193, SA194, SA196, SA197, SA198",
       image: IndicatorSA190,
-      text: 'View Mounting Template for SA190 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
+      text: "View Mounting Template for SA190 Indicator",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1081720",
     },
+
     {
-      device: 'Indicator',
-      functions: "8236, 8237, 8238, 8242, 8249, 8265, 8266, 8267, 8268",
-      title: 'SA191 - Indicator',
-      image: IndicatorSA190,
-      text: 'View Mounting Template for SA191 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
+      device: "Indicator",
+      functions: "8212, 8230, PHR-NAC-82285",
+      title: "VN1 Escutcheon Indicator",
+      image: IndicatorVN1,
+      text: "View Mounting Template for SA197 Indicator",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1081719",
     },
-    {
-      device: 'Indicator',
-      functions: "8204, 8215, 8226, 8228, 8239, 8240, 8241, 8247, PHR-NAC-82285",
-      title: 'SA197 - Indicator',
-      image: IndicatorSA190,
-      text: 'View Mounting Template for SA197 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
-    },
-    {
-      device: 'Indicator',
-      functions: "8217, 8229, 8250, 8251, 8255, 8256",
-      title: 'SA194 - Indicator',
-      image: IndicatorSA190,
-      text: 'View Mounting Template for SA194 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
-    },
-    {
-      device: 'Indicator',
-      functions: "8206, 8213, 8216, 8224, 8231, 8237, 8249, 8257, 8265",
-      title: 'SA196 - Indicator',
-      image: IndicatorSA190,
-      text: 'View Mounting Template for SA196 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
-    },
-    {
-      device: 'Indicator',
-      functions: "8220, 8221, 8224, 8225, 8229, 8240, 8245, 8246",
-      title: 'SA198 - Indicator',
-      image: IndicatorSA190,
-      text: 'View Mounting Template for SA198 Indicator',
-      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1081720',
-    },
-    
   ],
   9200: [
     {
       device: "Standard",
-      title: "9200 Mortise Lock - Standard",
+      title: "9200 W/ Escutcheon Trims",
       image: Mortise9200,
-      text: "Download Template",
-      link: "/templates/9200-standard-template.pdf",
-      warning: "",
-      text1: "Installation Guide",
-      link1: "/templates/9200-installation-guide.pdf",
+      text: 'View Metal and Wood Doors Mounting Template Doc# 7057',
+      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1055660',
+      text1: "Escutcheon Trim (CE, LE1, TE) Exposed Screws Template Doc# 4556",
+      link1: "https://storage.googleapis.com/aa-americas/dam/AADSS1056042",
+      text2: "Escutcheon Trim (LE2) Concealed Screws Template Doc# 4557",
+      link2: "https://storage.googleapis.com/aa-americas/dam/AADSS1056023",
+      text3: "Escutcheon Trim (WT) AV Anti-Vandal Trim Option Surface Applied Template Doc# 4558",
+      link3: "https://storage.googleapis.com/aa-americas/dam/AADSS1056021",
+      text4: "Escutcheon Trim (LE1, LE3, LW1, CE, TE, KE1, KE3, KW1) Exposed Screws Template Doc# 7052",
+      link4: "https://storage.googleapis.com/aa-americas/dam/AADSS1056024",
+      text5: 'Escutcheon Trim (LE2, LE4, KE2, KE4) Concealed Screws Template Doc# 7053',
+      link5: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056025',
+      text6: 'Security Escutcheon Trim (LS and KS) (Security Screws) Spanner Head Template',
+      link6: 'https://storage.googleapis.com/aa-americas/dam/AADSS1056043',
+
+    },
+    {
+      device: 'Standard',
+      title: 'Strikes',
+      image: Strike9200,
+
+      text: 'Curved or Straight Lip Strikes (M-)9200 Template Doc# 7063',
+      link: 'https://storage.googleapis.com/aa-americas/dam/AADSS1055593',
+      text1: '78-0034 Deadbolt Strike (M-)9200 03, 20, 21, 22, and 23 Functions Template',
+      link1: 'https://storage.googleapis.com/aa-americas/dam/AADSS1055661',
     },
     {
       device: "Electrified",
@@ -150,6 +149,28 @@ export const MortiseLocks = {
       warning: "",
       text1: "Installation Guide",
       link1: "/templates/7800-installation-guide.pdf",
+    },
+
+    {
+      device: "Indicator",
+      warning:
+        "Right Click on picture -> Open image in new tab to see more details",
+      functions:
+        "7800, 7801, 7802, 7804, 7805, 7806, 7828, 7809, 7810, 7819, 7840, 7841, 7843, 7845, 7846, 7847, 7850, 7851, 7852",
+      title:
+        "Sectional Indicator SA190, SA191, SA192, SA193, SA194, SA196, SA197, SA198",
+      image: IndicatorSA190,
+      text: "View Mounting Template for SA190 Indicator",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1081720",
+    },
+
+    {
+      device: "Indicator",
+      functions: "8212, 8230, PHR-NAC-82285",
+      title: "VN1 Escutcheon Indicator",
+      image: IndicatorVN1,
+      text: "View Mounting Template for SA197 Indicator",
+      link: "https://storage.googleapis.com/aa-americas/dam/AADSS1081719",
     },
     // No Electrified option for 7800
   ],
