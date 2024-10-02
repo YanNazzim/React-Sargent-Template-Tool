e741538727e935783bfda045186e192c6b68b15b
Yan <69820754+YanNazzim@users.noreply.github.com>
2024-10-02 14:07:50 -0400

    Put some animation and functionality -- Logo/temps

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,13 @@ function Header() {
     navigate("/"); // Navigate to the home page
   };
 
+  const handleButtonClickLogo = () => {
+    window.open("https://www.sargentlock.com/en", "_blank"); // Open in a new tab
+  };
+
   return (
     <header className='header'>
-      <img src={logo} alt='Sargent Logo' className='SargentLogo'/>
+      <img src={logo} alt='Sargent Logo' className='SargentLogo' onClick={handleButtonClickLogo}/>
       <button className="Home" onClick={handleButtonClickHome}>Templates Home Page</button>
       <button className="Home" onClick={handleButtonClickBack}>Previous Page</button>
     </header>
