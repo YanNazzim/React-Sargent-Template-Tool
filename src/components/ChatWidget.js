ee61be36c341dad2a315ed9be375ee3d6697b199
Yan <69820754+YanNazzim@users.noreply.github.com>
2025-12-30 17:25:33 -0500

    fixed error

--- src/components/ChatWidget.js
+++ src/components/ChatWidget.js
@@ -1,6 +1,6 @@
 // src/components/ChatWidget.js
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import "../components/ChatWidget.css";
+import "../components/style/ChatWidget.css";
 
 const ChatWidget = () => {
   const [isOpen, setIsOpen] = useState(false);
